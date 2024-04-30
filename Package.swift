7f4688dc97fda9bfdd0b6bf4ef0b9161f069963c
Facephi <developer@facephi.com>
2024-04-30 10:07:31 +0200

    New SPM Version: 2.0.4

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SDK-StatusPackage-SPM",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,7 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", from: "1.5.7"),
+        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .upToNextMinor(from: "2.0.0")),
+        .package(url: "git@github.com:airbnb/lottie-spm", .upToNextMinor(from: "4.4.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -22,9 +24,9 @@ let package = Package(
         .target(
             name: "SDK-StatusPackage-SPM",
             dependencies: [
-                .product(name: "core", package: "SDK-CorePackage-SPM")
-            ],
-            resources: [.process("Resources")]),
+                .product(name: "core", package: "SDK-CorePackage-SPM"),
+                "lottie-spm"
+            ]),
         .binaryTarget(name: "statusComponent", path: "statusComponent.xcframework"),
         
     ]
